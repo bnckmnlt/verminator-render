99014031e64b332d8cbf035cdd97efb41ffbca96
Bry <bryanmontealtomain@gmail.com>
2025-05-13 21:06:07 +0800

    feat:added worm activity route

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,4 +1,4 @@
 export { default as compostCycle } from "./compost-cycle";
 export { layerType, default as sensorReadings, sensorReadingsRelations } from "./sensor-readings";
-export { cycleStatus, default as statusRecords } from "./status-records";
+export { scheduleStatus, default as statusRecords } from "./status-records";
 export { default as wormActivity } from "./worm-activity";
